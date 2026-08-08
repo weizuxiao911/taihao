508929133ba2ab1275ed8eff0ef5dd020f9e6254
weizuxiao <727897965@qq.com>
2026-08-09 02:15:37 +0800

    fix(buildroot-pi4b): 补 TAIHAO metapackage + post-image genimage 路径修复(产出 sdcard.img)

--- packaging/buildroot-pi4b/scripts/post-image.sh
+++ packaging/buildroot-pi4b/scripts/post-image.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-BOARD_DIR="$(dirname $0)"
+BOARD_DIR="$(dirname $0)/../board/raspberrypi4-64"
 BOARD_NAME="raspberrypi4-64"
 GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
